d69a49a5609b1c78eb8df5eecc02cc61faa59a17
liaoxuan <arthur.liao@huolala.cn>
2023-09-06 17:21:34 +0800

    feat: improve docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Quaere</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Quaere Docs',
+    text: "Quaere Docs",
   },
   i18n: [
-    { locale: 'en', text: 'English' },
-    { locale: 'zh', text: '中文' },
-  ]
-}
+    { locale: "en", text: "English" },
+    { locale: "zh", text: "中文" },
+  ],
+  search: {
+    placeholder: "Search docs",
+  },
+};
 
-export default config
+export default config;
